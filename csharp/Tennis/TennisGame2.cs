fc8f82b2d7998adfd7bc38f0d6a081cede6bb84f
Susana Ruiz <susana.r.cunado@gmail.com>
2025-03-08 12:24:28 +0100

    TG2/C#/0002 - Move score name logic to separated method.

--- csharp/Tennis/TennisGame2.cs
+++ csharp/Tennis/TennisGame2.cs
@@ -2,18 +2,15 @@ namespace Tennis
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int p1point = 0;
+        private int p2point = 0;
 
-        private string p1res = "";
-        private string p2res = "";
         private string player1Name;
         private string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
         }
 
@@ -22,84 +19,35 @@ namespace Tennis
             var score = "";
             if (p1point == p2point && p1point < 3)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
+                score = $"{ScoreName(p1point)}-All";
             }
-            if (p1point == p2point && p1point > 2)
+            else if (p1point == p2point && p1point > 2)
+            {
                 score = "Deuce";
-
-            if (p1point > 0 && p2point == 0)
-            {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
             }
-            if (p2point > 0 && p1point == 0)
+            else if (p1point < 4 && p2point < 4)
             {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
+                score = ScoreName(p1point) + "-" + ScoreName(p2point);
             }
-
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
+            else if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
             {
                 score = "Win for player1";
             }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
+            else if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
             {
                 score = "Win for player2";
+            }
+            else if (p1point > p2point && p2point >= 3)
+            {
+                score = "Advantage player1";
+            }
+            else if (p2point > p1point && p1point >= 3)
+            {
+                score = "Advantage player2";
+            }
+            else
+            {
+                score = "";
             }
             return score;
         }
@@ -136,6 +84,17 @@ namespace Tennis
                 P1Score();
             else
                 P2Score();
+        }
+        private string ScoreName(int points)
+        {
+            return points switch
+            {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => "Invalid Score"
+            };
         }
 
     }
